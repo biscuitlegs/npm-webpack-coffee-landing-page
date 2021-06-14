c11e3f93078ce6efe0c791968e70e491b4c65622
Bradley <bmcguigandev@outlook.com>
2021-06-14 16:01:40 +0100

    Add navbar brand

--- src/navbar.js
+++ src/navbar.js
@@ -7,9 +7,12 @@ navbar.innerHTML = `<div class="container-fluid">
                       </button>
                       <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                          <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
-                          </li>
+                          <a class="navbar-brand text-success fw-bold" href="#">
+                            <svg class="pb-1" xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-cup" viewBox="0 0 16 16">
+                              <path d="M1 2a1 1 0 0 1 1-1h11a1 1 0 0 1 1 1v1h.5A1.5 1.5 0 0 1 16 4.5v7a1.5 1.5 0 0 1-1.5 1.5h-.55a2.5 2.5 0 0 1-2.45 2h-8A2.5 2.5 0 0 1 1 12.5V2zm13 10h.5a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.5-.5H14v8zM13 2H2v10.5A1.5 1.5 0 0 0 3.5 14h8a1.5 1.5 0 0 0 1.5-1.5V2z"/>
+                            </svg>
+                            Bob's Barista
+                          </a>
                           <li class="nav-item">
                             <a class="nav-link" href="#">Link</a>
                           </li>
